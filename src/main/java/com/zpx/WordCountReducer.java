5c11f42044afdecc830f22e1d95ac6efcaefd5cd
zpxde <2141272383@qq.com>
2022-04-09 22:27:40 +0800

    更新

---
+++ src/main/java/com/zpx/WordCountReducer.java
@@ -0,0 +1,29 @@
+package com.zpx;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * KEYIN, reduce阶段输入的key的类型：Text
+ * VALUEIN,reduce阶段输入value类型：IntWritable
+ * KEYOUT,reduce阶段输出的Key类型：Text
+ * VALUEOUT,reduce阶段输出的value类型：IntWritable
+ */
+
+public class WordCountReducer extends Reducer<Text,IntWritable,Text,IntWritable>{
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        //(1.1)
+        int sum = 0;
+        for (IntWritable value : values) {
+            sum+=value.get();
+        }
+        IntWritable outV = new IntWritable();
+        outV.set(sum);
+        //写出
+        context.write(key,outV);
+    }
+}
